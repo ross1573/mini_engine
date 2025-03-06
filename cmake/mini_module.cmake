6e800926a479c85c1fa43f7fc75dbd8f7315365a
ross1573 <ross1573@naver.com>
2025-03-06 15:42:05 +0900

    add: cmake function module_sources
    this will simplify target_sources and build_source_tree into a single function

--- cmake/mini_module.cmake
+++ cmake/mini_module.cmake
@@ -1,27 +1,3 @@
-function (add_module name type)
-    string(REGEX REPLACE "mini[._]" "" api ${name})
-    string(REPLACE "." "_" api ${api})
-    string(REPLACE "." "_" header ${name})
-
-    set(api ${api}_API)
-    set(header "${header}.generated.h")
-    string(TOUPPER ${api} api)
-
-    add_library(${name} ${type})
-
-    generate_export_header(${name}
-        EXPORT_MACRO_NAME ${api}
-        EXPORT_FILE_NAME ${header}
-    )
-
-    target_precompile_headers(${name}
-    PUBLIC
-        ${CMAKE_CURRENT_BINARY_DIR}/${header}
-    )
-
-    set_target_properties(${name} PROPERTIES FOLDER modules)
-endfunction()
-
 function (build_source_tree name)
     get_target_property(sources ${name} SOURCES)
     get_target_property(headers ${name} HEADERS)
@@ -52,5 +28,38 @@ function (build_source_tree name)
         foreach(config_type ${CMAKE_CONFIGURATION_TYPES})
             source_group("Generated Files" FILES ${cmake_gen_path}/${config_type}/cmake_pch.hxx)
         endforeach()
+    endif()
+endfunction()
+
+function (module_sources name)
+    target_sources(${ARGV})
+    build_source_tree(${name})
+endfunction()
+
+function (add_module name type)
+    string(REGEX REPLACE "mini[._]" "" api ${name})
+    string(REPLACE "." "_" api ${api})
+    string(REPLACE "." "_" header ${name})
+
+    set(api ${api}_API)
+    set(header "${header}.generated.h")
+    string(TOUPPER ${api} api)
+
+    add_library(${name} ${type})
+
+    generate_export_header(${name}
+        EXPORT_MACRO_NAME ${api}
+        EXPORT_FILE_NAME ${header}
+    )
+
+    target_precompile_headers(${name}
+    PUBLIC
+        ${CMAKE_CURRENT_BINARY_DIR}/${header}
+    )
+
+    set_target_properties(${name} PROPERTIES FOLDER modules)
+
+    if (${ARGC} GREATER 2)
+        module_sources(${name} ${ARGN})
     endif()
 endfunction()
