6c08b26a04591944b2ad7c8013975778a382d5fd
ross1573 <ross1573@naver.com>
2025-04-01 01:11:40 +0900

    fix: unable to break out from the CFRunLoop

--- engine/source/macos/cocoa/application_delegate.mm
+++ engine/source/macos/cocoa/application_delegate.mm
@@ -34,5 +34,24 @@ void ApplicationDelegate::Run()
 
 void ApplicationDelegate::Stop()
 {
+    NSEvent* event = nullptr;
+    @autoreleasepool {
+        event = [NSEvent otherEventWithType:NSEventTypeApplicationDefined
+                                   location:NSMakePoint(0, 0)
+                              modifierFlags:0
+                                  timestamp:0
+                               windowNumber:0
+                                    context:nil
+                                    subtype:0
+                                      data1:0
+                                      data2:0];
+    }
+
+    if (event == nullptr) {
+        [NSApp terminate:nil];
+        return;
+    }
+
+    [NSApp postEvent:event atStart:YES];
     [NSApp stop:nil];
 }
