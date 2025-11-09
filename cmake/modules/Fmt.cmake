07c3761dca34c07edd2c896f528b231630ec1037
ross1573 <ross1573@naver.com>
2025-11-09 16:37:40 +0900

    mod: update fmt to 12.1.0

--- cmake/modules/Fmt.cmake
+++ cmake/modules/Fmt.cmake
@@ -14,7 +14,7 @@ set(FMT_UNICODE ON)
 FetchContent_Declare(
     fmt
     GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-    GIT_TAG 11.2.0
+    GIT_TAG 12.1.0
 )
 FetchContent_MakeAvailable(fmt)
 
