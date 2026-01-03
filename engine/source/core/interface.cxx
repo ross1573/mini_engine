2cc074851d2bba47176c5dbf088e421b1f8b7b79
ross1573 <ross1573@naver.com>
2026-01-04 03:16:20 +0900

    add: interface for core module

---
+++ engine/source/core/interface.cxx
@@ -0,0 +1,22 @@
+export module mini.core:interface;
+
+import :module_system;
+import :module_loader;
+
+namespace mini::core {
+
+class CORE_API Interface final : public Module::Interface {
+public:
+    Interface() noexcept;
+    ~Interface() noexcept final;
+
+private:
+    bool Initialize() final;
+};
+
+Interface::~Interface() noexcept
+{
+    ASSERT(g_ModuleLoader.Count() == 0, "module remaining");
+}
+
+} // namespace mini::core
