daf152fec0889950ed3da90cd087162ebc38745e
ross1573 <ross1573@naver.com>
2026-03-02 16:02:12 +0900

    [launcher] mod: simplified winmain entry

--- engine/source/launcher/windows/win_main.cpp
+++ engine/source/launcher/windows/win_main.cpp
@@ -1,14 +1,10 @@
-#define WIN32_LEAN_AND_MEAN
-#define VC_EXTRALEAN
-#include <windows.h>
-
 #if DEBUG
 #  include <crtdbg.h>
 #endif
 
 import mini.launcher;
 
-int APIENTRY wWinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPWSTR, _In_ int)
+extern "C" int __stdcall wWinMain(void*, void*, wchar_t*, int)
 {
 #if DEBUG
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
