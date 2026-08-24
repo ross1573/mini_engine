74e82700aa1395ed1d05ae2e56a96311dd4a2c66
ross1573 <ross1573@naver.com>
2026-08-25 03:16:36 +0900

    [core] moved assert format functions to seperate cpp file

--- engine/source/core/debug/assertion_format.cxx
+++ engine/source/core/debug/assertion_format.cxx
@@ -21,15 +21,8 @@ export struct ASSERT_API AssertFormatString {
 public:
     char const* data;
     size_t size;
-    static constexpr char const empty[1] = { '\0' };
 
 public:
-    constexpr AssertFormatString()
-        : data(empty)
-        , size(0)
-    {
-    }
-
     constexpr AssertFormatString(char const* str, size_t len)
         : data(str)
         , size(len)
@@ -46,7 +39,6 @@ public:
 export struct ASSERT_API AssertFormatResult {
 public:
     char const* message;
-    char const* location;
 };
 
 export struct ASSERT_API AssertFormatContext {
@@ -56,7 +48,7 @@ public:
 
 public:
     AssertFormatContext(SourceLocation loc = SourceLocation::current())
-        : message()
+        : message(nullptr, 0)
         , location(loc)
     {
     }
@@ -81,122 +73,23 @@ public:
     }
 };
 
-[[no_inline]] ASSERT_API constexpr size_t ConcatStrings(char* dest, size_t destLen, char const** src, size_t srcCount)
-{
-    if (src == nullptr || srcCount == 0) {
-        return 0;
-    }
-
-    char* begin = dest;
-    for (size_t i = 0; i < srcCount && destLen > 0; ++i) {
-        if (src[i] == nullptr) {
-            continue;
-        }
-
-        size_t len = memory::StringLength(src[i], destLen - 1);
-        memory::MemCopy(dest, src[i], len);
-        dest += len;
-        destLen -= len;
-    }
-
-    *dest = '\0';
-    return static_cast<size_t>(dest - begin);
-}
-
-ASSERT_API constexpr size_t IntegerToASCII(char* dest, size_t destLen, int src)
-{
-    size_t len = 0;
-    char buf[10];
-    for (; src > 0; ++len) {
-        buf[len] = '0' + src % 10;
-        src /= 10;
-    }
-
-    size_t min = len < destLen ? len : destLen;
-    for (size_t i = 0; i < min; ++i) {
-        dest[i] = buf[(len - 1) - i];
-    }
-
-    return len;
-}
-
-[[no_inline]] ASSERT_API constexpr size_t SourceLocationToString(char* dest, size_t destLen, SourceLocation const& loc)
-{
-    char* begin = dest;
-    char const* funcName = loc.function_name();
-    size_t funcLen = memory::StringLength(funcName, destLen - 1);
-
-    funcLen = destLen > funcLen ? funcLen : destLen;
-    memory::MemCopy(dest, funcName, funcLen);
-    dest += funcLen;
-    destLen -= funcLen;
-
-    char const* fileName = loc.file_name();
-    size_t fileLen = memory::StringLength(fileName, destLen - 1);
-    if (destLen > fileLen + 4) {
-        memory::MemCopy(dest, " (", 2);
-        memory::MemCopy(dest + 2, fileName, fileLen);
-        dest += fileLen + 2;
-        destLen -= fileLen + 2;
-
-        char lineBuf[16];
-        size_t lineLen = IntegerToASCII(lineBuf, sizeof(lineBuf), (int)loc.line());
-        if (destLen > lineLen + 2) {
-            dest[0] = ':';
-            memory::MemCopy(dest + 1, lineBuf, lineLen);
-            dest += lineLen + 1;
-        }
-
-        dest[0] = ')';
-        ++dest;
-    }
-
-    *dest = '\0';
-    return static_cast<size_t>(dest - begin);
-}
-
-ASSERT_API constexpr char const assertMessageFormat[] = "\n- Message: ";
-ASSERT_API constexpr char const assertMessageSuffix[] = "\n\n" ENGINE_PROJECT_NAME ":" ENGINE_PROJECT_VERSION
-                                                        ":" ENGINE_BUILD_CONFIG " " ENGINE_BUILD_ARCH
-                                                        ":" ENGINE_BUILD_COMPILER " ";
-ASSERT_API constexpr char const assertFormat[] = "\n\nAssertion failed!\n"
-                                                 "- Expression: {0}\n"
-                                                 "- Function: {1}";
-
-constexpr size_t assertSuffixSize = sizeof(assertMessageSuffix);
 constexpr size_t assertBufferRawSize = 1024;
-constexpr size_t assertBufferSize = NOASSERT ? 1 : assertBufferRawSize - assertSuffixSize;
 
+[[no_inline]] ASSERT_API AssertFormatResult AssertFormat(char const* expr, char const* msg, SourceLocation loc);
 [[no_inline]] ASSERT_API void LogAssert(AssertFormatResult formatResult);
 [[no_inline]] ASSERT_API void LogEnsure(AssertFormatResult formatResult);
 
 export template <typename... Args>
 constexpr AssertFormatResult AssertFormat(char const* expr, AssertFormatContext ctx, Args&&... args)
 {
-    thread_local char assertBuffer[assertBufferRawSize] = { 0 };
-    thread_local char locationBuffer[assertBufferSize] = { 0 };
-    char* buffer = assertBuffer;
-    char const* bufferEnd = buffer + assertBufferSize;
-
-    SourceLocationToString(locationBuffer, assertBufferSize, ctx.location);
-    auto result = fmt::format_to_n(buffer, assertBufferSize, assertFormat, expr, locationBuffer);
-    buffer += result.size;
-
-    if (ctx.message.size == 0 || buffer >= bufferEnd) {
-        memory::MemCopy(buffer, assertMessageSuffix, assertSuffixSize);
-        return AssertFormatResult{ .message = assertBuffer, .location = locationBuffer };
+    if constexpr (sizeof...(Args) == 0) {
+        return AssertFormat(expr, ctx.message.data, ctx.location);
     }
 
-    memory::MemCopy(buffer, assertMessageFormat, sizeof(assertMessageFormat) - 1);
-    buffer += sizeof(assertMessageFormat) - 1;
-
+    char fmtBuffer[assertBufferRawSize] = { 0 };
     auto fmtMessage = fmt::string_view(ctx.message.data, ctx.message.size);
-    size_t remaining = static_cast<size_t>(bufferEnd - buffer);
-    result = fmt::vformat_to_n(buffer, remaining, fmtMessage, fmt::make_format_args(args...));
-    buffer += result.size;
-
-    memory::MemCopy(buffer, assertMessageSuffix, assertSuffixSize);
-    return AssertFormatResult{ .message = assertBuffer, .location = locationBuffer };
+    fmt::vformat_to_n(fmtBuffer, assertBufferRawSize, fmtMessage, fmt::make_format_args(args...));
+    return AssertFormat(expr, fmtBuffer, ctx.location);
 }
 
 export template <typename... Args>
@@ -205,9 +98,9 @@ export template <typename... Args>
     LogAssert(AssertFormat(expr, ctx, ForwardArg<Args>(args)...));
 }
 
-export [[no_inline]] ASSERT_API void LogAssert(char const* expr)
+export [[no_inline]] ASSERT_API void LogAssert(char const* expr, SourceLocation loc = SourceLocation::current())
 {
-    LogAssert(AssertFormat(expr, AssertFormatContext()));
+    LogAssert(AssertFormat(expr, nullptr, loc));
 }
 
 export template <typename... Args>
@@ -216,9 +109,9 @@ export template <typename... Args>
     LogEnsure(AssertFormat(expr, ctx, ForwardArg<Args>(args)...));
 }
 
-export [[no_inline]] ASSERT_API void LogEnsure(char const* expr)
+export [[no_inline]] ASSERT_API void LogEnsure(char const* expr, SourceLocation loc = SourceLocation::current())
 {
-    LogEnsure(AssertFormat(expr, AssertFormatContext()));
+    LogEnsure(AssertFormat(expr, nullptr, loc));
 }
 
 } // namespace mini::debug
---
+++ engine/source/core/debug/impl/assertion_format.cpp
@@ -0,0 +1,168 @@
+module mini.core;
+
+import :type;
+import :cmemory;
+import :string_memory;
+import :assert_format;
+import :source_location;
+
+namespace mini::debug {
+
+constexpr char const assertStrTitle[] = "\n\nAssertion failed!\n";
+constexpr char const assertStrExpr[] = "- Expression: ";
+constexpr char const assertStrFunc[] = "- Function: ";
+constexpr char const assertStrMsg[] = "- Message: ";
+
+constexpr char const assertSuffix[] = "\n\n" ENGINE_PROJECT_NAME ":" ENGINE_PROJECT_VERSION ":" ENGINE_BUILD_CONFIG
+                                      " " ENGINE_BUILD_ARCH ":" ENGINE_BUILD_COMPILER "\n";
+
+constexpr size_t assertSuffixSize = sizeof(assertSuffix);
+constexpr size_t assertBufferSize = NOASSERT ? 1 : assertBufferRawSize - assertSuffixSize;
+constexpr size_t assertRequiredSize = sizeof(assertStrTitle) + sizeof(assertStrExpr) + sizeof(assertStrFunc) +
+                                      sizeof(assertStrMsg) + sizeof(assertSuffix);
+
+static_assert(assertBufferRawSize > assertRequiredSize);
+
+ASSERT_API thread_local char assertBuffer[assertBufferRawSize] = { 0 };
+
+size_t FormatASCII(char* dest, size_t destLen, int src)
+{
+    size_t len = 0;
+    char buf[10];
+    for (; src > 0; ++len) {
+        buf[len] = '0' + src % 10;
+        src /= 10;
+    }
+
+    size_t min = len < destLen ? len : destLen;
+    for (size_t i = 0; i < min; ++i) {
+        dest[i] = buf[(len - 1) - i];
+    }
+
+    return len;
+}
+
+size_t FormatSourceLocation(char* dest, char const* destEnd, SourceLocation const& loc)
+{
+    char* begin = dest;
+    size_t destLen = static_cast<size_t>(destEnd - dest);
+    if (dest == destEnd) {
+        return 0;
+    }
+
+    char const* funcName = loc.function_name();
+    size_t funcLen = memory::StringLength(funcName);
+    funcLen = destLen > funcLen ? funcLen : destLen;
+
+    memory::MemCopy(dest, funcName, funcLen);
+    dest += funcLen;
+    destLen -= funcLen;
+
+    char const* fileName = loc.file_name();
+    size_t fileLen = memory::StringLength(fileName);
+    if (destLen > fileLen + 4) {
+        memory::MemCopy(dest, " (", 2);
+        memory::MemCopy(dest + 2, fileName, fileLen);
+        dest += fileLen + 2;
+        destLen -= fileLen + 2;
+
+        char lineBuf[16];
+        size_t lineLen = FormatASCII(lineBuf, sizeof(lineBuf), (int)loc.line());
+        if (destLen > lineLen + 2) {
+            dest[0] = ':';
+            memory::MemCopy(dest + 1, lineBuf, lineLen);
+            dest += lineLen + 1;
+        }
+
+        dest[0] = ')';
+        ++dest;
+    }
+
+    return static_cast<size_t>(dest - begin);
+}
+
+size_t FormatAssertLine(char* dest, char const* destEnd, char const** src, size_t srcCount)
+{
+    size_t destLen = static_cast<size_t>(destEnd - dest) - 1;
+    if (dest == destEnd || src == nullptr || srcCount == 0) {
+        return 0;
+    }
+
+    char* begin = dest;
+    for (size_t i = 0; i < srcCount && destLen > 0; ++i) {
+        if (src[i] == nullptr) {
+            continue;
+        }
+
+        size_t len = memory::StringLength(src[i]);
+        len = destLen > len ? len : destLen;
+
+        memory::MemCopy(dest, src[i], len);
+        dest += len;
+        destLen -= len;
+    }
+
+    *(dest++) = '\n';
+    return static_cast<size_t>(dest - begin);
+}
+
+size_t FormatAssertString(char* dest, char const* destEnd, char const* src)
+{
+    size_t destLen = static_cast<size_t>(destEnd - dest);
+    if (dest == destEnd || src == nullptr) {
+        return 0;
+    }
+
+    char* begin = dest;
+    size_t len = memory::StringLength(src);
+    len = destLen > len ? len : destLen;
+
+    memory::MemCopy(dest, src, len);
+    dest += len;
+    destLen -= len;
+
+    return static_cast<size_t>(dest - begin);
+}
+
+AssertFormatResult AssertFormat(char const* expr, char const* message, SourceLocation location)
+{
+    size_t endLineCount = message != nullptr ? 1 : 0;
+    char* buffer = assertBuffer;
+    char* bufferEnd = buffer + assertBufferSize - endLineCount;
+    char const* srcBuffer[2];
+
+    buffer += FormatAssertString(buffer, bufferEnd, assertStrTitle);
+    if (buffer == bufferEnd) {
+        goto format_result;
+    }
+
+    srcBuffer[0] = assertStrExpr;
+    srcBuffer[1] = expr;
+    buffer += FormatAssertLine(buffer, bufferEnd, srcBuffer, 2);
+    if (buffer == bufferEnd) {
+        goto format_result;
+    }
+
+    buffer += FormatAssertString(buffer, bufferEnd, assertStrFunc);
+    if (buffer == bufferEnd) {
+        goto format_result;
+    }
+
+    buffer += FormatSourceLocation(buffer, bufferEnd, location);
+    if (buffer == bufferEnd) {
+        goto format_result;
+    }
+
+    if (message != nullptr) {
+        *(buffer++) = '\n';
+        ++bufferEnd;
+        buffer += FormatAssertString(buffer, bufferEnd, assertStrMsg);
+        buffer += FormatAssertString(buffer, bufferEnd, message);
+    }
+
+format_result:
+    memory::MemCopy(buffer, assertSuffix, assertSuffixSize);
+    return AssertFormatResult{ .message = assertBuffer };
+}
+
+} // namespace mini::debug
