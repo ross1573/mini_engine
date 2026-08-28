dd70f95b9d9e94f6873ec391b9cf93b1092a3729
ross1573 <ross1573@naver.com>
2026-08-28 22:35:08 +0900

    update google-benchmark to 1.9.5
    this will fix c2y extension related warnings

--- cmake/external/GoogleBenchmark.cmake
+++ cmake/external/GoogleBenchmark.cmake
@@ -18,7 +18,7 @@ set(BENCHMARK_USE_BUNDLED_GTEST OFF)
 FetchContent_Declare(
     googlebenchmark
     GIT_REPOSITORY https://github.com/google/benchmark.git
-    GIT_TAG v1.9.4
+    GIT_TAG v1.9.5
 )
 FetchContent_MakeAvailable(googlebenchmark)
 
